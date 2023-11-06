922e64a1c307ed90a00a63dff83dcae2c26f5a93
RiaJul Pro <riajulpro@outlook.com>
2023-11-06 07:31:59 +0600

    Creating a collection and having data from the database

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: [],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
@@ -34,9 +29,20 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // Database List
+    const allAssignments = client
+      .db("assignmentDB")
+      .collection("allAssignments");
+
     // Reading Data
     app.get("/", (req, res) => {
       res.send("Online Group Study Platform");
+    });
+
+    app.get("/assignments", async (req, res) => {
+      const cursor = allAssignments.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
