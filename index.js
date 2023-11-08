9958b05dfa90c06dd9097002f0ed68dda320af9f
RiaJul Pro <riajulpro@outlook.com>
2023-11-08 21:54:05 +0600

    Remove client connect function for deploying

--- index.js
+++ index.js
@@ -37,9 +37,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
     // Database List
     const allAssignments = client
       .db("assignmentDB")
