fddf3fb76ed6c736a235e61d8ef187b08aab33b4
RiaJul Pro <riajulpro@outlook.com>
2023-11-08 23:03:43 +0600

    Adding secure and sameSite property with cookie function

--- index.js
+++ index.js
@@ -52,8 +52,12 @@ async function run() {
         expiresIn: "24h",
       });
       res
-        .cookie("token", token, { httpOnly: true })
-        .send({ msg: "Succeed", token });
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
     });
 
     // JWT Middleware
